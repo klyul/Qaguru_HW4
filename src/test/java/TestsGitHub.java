bbc4ae140ee6d18d219b16e25199c532730c0190
klyul <yuliya.v3333@gmail.com>
2023-02-24 14:13:28 +0300

    hw4

---
+++ src/test/java/TestsGitHub.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+public class TestsGitHub {
+    @Test
+    void ExampleSoftAssertions() {
+        open("https://github.com/selenide/selenide");
+        $("nav.js-repo-nav").$("#wiki-tab").click();
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $("div.wiki-rightbar").$$("div ul").first().shouldHave(text("SoftAssertions"));
+        $x("//a[text()='SoftAssertions']").click();
+        $("div.markdown-body").shouldHave(text("Using JUnit5 extend test class:"));
+    }
+}
+
